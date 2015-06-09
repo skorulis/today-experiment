9c36a8f14076e0cb5b30ee292d9a8ff71f383d92
Alexander Skorulis <skorulis@gmail.com>
2015-06-09 11:18:47 +1000

    Work on a nice looking login form.

--- src/today.js
+++ src/today.js
@@ -1,28 +1,5 @@
 Parse.initialize("obDHGL7K1Fmen951R5ezXvECakKrVVeCg3fNwLYg", "MSgkDUlpCg4mlDNcva1VjAcZoLVXLHPBHA286omP");
 
-var RegisterElement = React.createClass({
-	handleSubmit: function(e) {
-    	e.preventDefault();
-    	var username = React.findDOMNode(this.refs.username).value.trim();
-    	var password = React.findDOMNode(this.refs.password).value.trim();
-    	var email = React.findDOMNode(this.refs.email).value.trim();
-    	this.props.onRegisterSubmit(username,password,email)
-		return;
-	},
-	render: function() {
-    return (
-      <div className="register">
-		<form onSubmit={this.handleSubmit}>
-			<input type="text" placeholder="username" ref="username" />
-			<input type="text" placeholder="password" ref="password" />
-			<input type="text" placeholder="email" ref="email" />
-			<input type="submit" value="Register" />
-			</form>
-      </div>
-    );
-  }
-});
-
 var PageContent = React.createClass({
 	handleRegisterSubmit: function(username,password,email) {
 		console.log("register")
