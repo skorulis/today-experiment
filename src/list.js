17b52f0b4292112e8e48e8a028c56d6a0d650e31
Alexander Skorulis <skorulis@gmail.com>
2015-06-09 15:13:26 +1000

    Work on being able to take a video capture.

--- src/list.js
+++ src/list.js
@@ -1,8 +1,57 @@
 
+navigator.getMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+function startRecording() {
+    var video = document.querySelector('#video')
+    var streaming = false
+    
+    video.addEventListener('canplay', function(ev){
+    if (!streaming) {
+      height = video.videoHeight / (video.videoWidth/width);
+      video.setAttribute('width', width);
+      video.setAttribute('height', height);
+      streaming = true;
+    }
+  }, false);
+    
+    navigator.getMedia(
+    { video: true, audio: false },
+    function(stream) {
+      if (navigator.mozGetUserMedia) {
+        video.mozSrcObject = stream;
+      } else {
+        var vendorURL = window.URL || window.webkitURL;
+        video.src = vendorURL.createObjectURL(stream);
+      }
+      video.play();
+    },
+    function(err) {
+      console.log("An error occured! " + err);
+    }
+  );
+
+}
+
+
 var ListItem = React.createClass({
+    startCapturePressed: function(e) {
+      e.preventDefault()
+      startRecording()
+      console.log("TEST")
+    },
     render: function() {
+        var videoElement
+        
+        if(this.props.day.get("date") === dateToday()) {
+            videoElement = <div>
+                <video id="video" /><button id="startbutton" onClick={this.startCapturePressed}>Start capture</button>
+                </div>
+        }
       return (
-         <li> TEST</li>
+            <li>
+                {this.props.day.get("date")} 
+                {videoElement}
+            </li>
       )}
 });
 
@@ -16,7 +65,6 @@ var ContentList = React.createClass({
         today.set("date",dateToday())
         today.set("parent",this.props.user)
         today.save()
-        console.log("TEST")
     },
     
     componentDidMount: function() {
@@ -25,7 +73,7 @@ var ContentList = React.createClass({
         query.equalTo("parent",this.props.user)
         query.find({
             success: function(results) {
-                console.log("found " + results)     
+                console.table(results)     
                 component.setState({days:results})
             },
             error: function(error) {
@@ -41,7 +89,7 @@ var ContentList = React.createClass({
         <a href="#" onClick={this.addDayPressed}><i className="fa fa-plus-circle"></i></a>
         <ul> {this.props.user.get("days")} 
         {this.state.days.map(function(day,i) {
-            return <ListItem />
+            return <ListItem day={day} key={day.get("date")} />
         })}
     
         </ul>
